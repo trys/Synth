78c792566b935e22e4e983736b779da38406841e
Trys <trysmudford@gmail.com>
2015-03-17 20:55:19 +0000

    Basic Envelope

--- synth.js
+++ synth.js
@@ -4,12 +4,13 @@ var Synth = (function () {
 	var keyboard = document.getElementById( 'keyboard' ),
 		context = new AudioContext(),
 		masterGain = context.createGain(),
-		osOneOutputGain = context.createGain(),
-		osSum = context.createGain(),
+		chOneOutputGain = context.createGain(),
+		chSum = context.createGain(),
+		Envelope,
 		VCO,
-		osOneSettings = {
+		VCA,
+		chOneSettings = {
 			wave: 'sine',
-			pitch: 1,
 			detune: 0
 		},
 		notes = [];
@@ -28,14 +29,29 @@ var Synth = (function () {
 
 			Synth.Define.Concepts();
 
-			osOneOutputGain.connect( osSum );
-			osSum.connect( masterGain );
+			chOneOutputGain.connect( chSum );
+			chSum.connect( masterGain );
 
 			masterGain.gain.value = 0.5;
 			masterGain.connect( context.destination );
 
 			keyboard.keyDown = Synth.KeyDown;
-			keyboard.keyUp = Synth.KeyUp;
+
+
+
+			var chOneVCO = new VCO,
+				chOneVCA = new VCA,
+				chOneEnvelope = new Envelope;
+
+			chOneVCO.setType( chOneSettings.wave );
+			chOneVCO.setDetune( chOneSettings.detune );
+
+			chOneEnvelope.setAttack( 0.5 );
+			chOneEnvelope.setRelease( 1 );
+
+			chOneVCO.connect( chOneVCA );
+			chOneEnvelope.connect( chOneVCA.amplitude );
+			chOneVCA.connect( chOneOutputGain );
 
 		},
 
@@ -44,6 +60,8 @@ var Synth = (function () {
 			Concepts: function () {
 
 				Synth.Define.VCO();
+				Synth.Define.Envelope();
+				Synth.Define.VCA();
 
 			},
 
@@ -59,6 +77,11 @@ var Synth = (function () {
 
 						this.input = this.oscillator;
 						this.output = this.oscillator;
+
+						var that = this;
+						document.body.addEventListener( 'frequency', function ( data ) {
+							that.setFrequency( data.detail );
+						});
 
 					};
 
@@ -87,35 +110,91 @@ var Synth = (function () {
 					return VCO;
 				})(context);
 
+			},
+
+			Envelope: function () {
+
+				Envelope = ( function( context ) {
+
+					function Envelope() {
+						this.attackTime = 0.1;
+						this.releaseTime = 0.1;
+
+						var that = this;
+						document.body.addEventListener( 'gate', function () {
+							that.trigger();
+						});
+
+					};
+
+					Envelope.prototype.setAttack = function ( attack ) {
+						this.attackTime = attack;
+					};
+
+					Envelope.prototype.setRelease = function ( release ) {
+						this.releaseTime = release;
+					};
+
+					Envelope.prototype.trigger = function() {
+						var now = context.currentTime;
+						this.param.cancelScheduledValues(now);
+						this.param.setValueAtTime(0, now);
+						this.param.linearRampToValueAtTime(1, now + this.attackTime);
+						this.param.linearRampToValueAtTime(0, now + this.attackTime + this.releaseTime);
+					};
+
+					Envelope.prototype.connect = function(param) {
+						this.param = param;
+					};
+
+					return Envelope;
+				})(context);
+
+			},
+
+			VCA: function () {
+
+				VCA = ( function( context ) {
+					function VCA() {
+						this.gain = context.createGain();
+						this.gain.gain.value = 0;
+						this.input = this.gain;
+						this.output = this.gain;
+						this.amplitude = this.gain.gain;
+					};
+
+					VCA.prototype.connect = function( node ) {
+						if ( node.hasOwnProperty( 'input' ) ) {
+							this.output.connect( node.input );
+						} else {
+							this.output.connect( node );
+						};
+					}
+
+					return VCA;
+				})(context);
+
 			}
 
 		},
 
 		KeyDown: function (note, frequency) {
 			
-			var osOne = new VCO;
-			
-			osOne.setType( osOneSettings.wave );
-			osOne.setFrequency( frequency * osOneSettings.pitch );
-			osOne.setDetune( osOneSettings.detune );
-
-			osOne.connect( osOneOutputGain );
-
-			if ( notes[ note ] !== undefined ) {
-				notes[ note ].oscillator.stop( 0 );
-				notes[ note ].oscillator.disconnect();
-			}
-
-			notes[ note ] = osOne;
+			Synth.Trigger( 'frequency', frequency );
+			Synth.Trigger( 'gate' );
 
 		},
-		
-		KeyUp: function ( note, frequency ) {
 
-			if ( notes[ note ] !== undefined ) {
-				notes[ note ].oscillator.stop( 0 );
-				notes[ note ].oscillator.disconnect();
+		Trigger: function ( event, data ) {
+
+			if ( window.CustomEvent ) {
+				var event = new CustomEvent( event, { detail: data } );
+			} else {
+				var event = document.createEvent( 'CustomEvent' );
+				event.initCustomEvent( event, true, true, data );
 			}
+
+			document.body.dispatchEvent( event );
 
 		}
 
