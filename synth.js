5f507a9200a58314bc74d9a66042c72e9a3c1edd
Trys <trysmudford@gmail.com>
2015-03-27 19:55:56 +0000

    Filter module added

--- synth.js
+++ synth.js
@@ -11,21 +11,34 @@ var Synth = (function () {
 		Envelope,
 		VCO,
 		VCA,
+		Filter,
 		chOneSettings = {
 			wave: 'sine',
 			detune: 0,
 			attack: 0,
-			release: 0.3,
+			release: 0.1,
 			sustain: 1,
-			pan: 0
+			pan: 0,
+			id: 0,
+			filter: {
+				frequency: 1500,
+				type: 'lowpass',
+				q: 10
+			}
 		},
 		chTwoSettings = {
 			wave: 'sine',
 			detune: 10,
-			attack: 0.3,
-			release: 0.3,
+			attack: 0.1,
+			release: 0.1,
 			sustain: 1,
-			pan: 1
+			pan: 1,
+			id: 1,
+			filter: {
+				frequency: 1500,
+				type: 'lowpass',
+				q: 10
+			}
 		};
 
 	return {
@@ -59,6 +72,7 @@ var Synth = (function () {
 				Synth.Define.VCO();
 				Synth.Define.Envelope();
 				Synth.Define.VCA();
+				Synth.Define.Filter();
 
 			},
 
@@ -69,7 +83,7 @@ var Synth = (function () {
 					function VCO() {
 						
 						this.oscillator = context.createOscillator();
-						this.oscillator.start( 0 );
+						this.oscillator.start( context.currentTime );
 
 						this.input = this.oscillator;
 						this.output = this.oscillator;
@@ -131,9 +145,7 @@ var Synth = (function () {
 						this.param.linearRampToValueAtTime(1, now + this.attackTime);
 						this.currentTime = now;
 
-						if ( this.sustain === 1 ) {
-
-						} else {
+						if ( this.sustain !== 1 ) {
 							this.param.linearRampToValueAtTime( 0, now + this.attackTime + this.releaseTime );
 						}
 					};
@@ -170,6 +182,46 @@ var Synth = (function () {
 					return VCA;
 				})(context);
 
+			},
+
+			Filter: function () {
+
+				Filter = ( function( context ) {
+
+					function Filter() {
+						
+						this.filter = context.createBiquadFilter();
+						
+						this.input = this.filter;
+						this.output = this.filter;
+
+						Filter.prototype.setFrequency = function ( frequency ) {
+							this.filter.frequency.value = frequency;
+						};
+
+						Filter.prototype.setType = function ( type ) {
+							this.filter.type = type;
+						};
+
+						Filter.prototype.setQ = function ( Q ) {
+							this.filter.Q.value = Q;
+						};
+
+					};
+
+					Filter.prototype.connect = function( node ) {
+						
+						if ( node.hasOwnProperty( 'input' ) ) {
+							this.output.connect( node.input );
+						} else {
+							this.output.connect( node );
+						};
+
+					}
+
+					return Filter;
+				})(context);
+
 			}
 
 		},
@@ -191,27 +243,33 @@ var Synth = (function () {
 
 			CreateOscillators: function ( frequency ) {
 
-				var CreatedOscillators = [];
+				var CreatedOscillators = {
+					oscillators : [],
+					merger : context.createChannelMerger( 2 )
+				}
 
 				if ( chOneSettings !== undefined ) {
-					CreatedOscillators.push( Synth.Channel.CreateOscillator( chOneSettings, frequency ) );
+					CreatedOscillators.oscillators.push( Synth.Channel.CreateOscillator( chOneSettings, frequency, CreatedOscillators ) );
 				}
 
 				if ( chTwoSettings !== undefined ) {
-					CreatedOscillators.push( Synth.Channel.CreateOscillator( chTwoSettings, frequency ) );
+					CreatedOscillators.oscillators.push( Synth.Channel.CreateOscillator( chTwoSettings, frequency, CreatedOscillators ) );
 				}
+
+				CreatedOscillators.merger.connect( chSum );
 
 				return CreatedOscillators;
 
 			},
 
-			CreateOscillator: function ( settings, frequency ) {
+			CreateOscillator: function ( settings, frequency, object ) {
 
 				var vco = new VCO,
 					vca = new VCA,
 					envelope = new Envelope,
 					pannerGain = context.createGain(),
-					panner = context.createPanner();
+					panner = context.createPanner(),
+					filter = new Filter;
 
 				vco.setType( settings.wave );
 				vco.setDetune( settings.detune );
@@ -222,13 +280,12 @@ var Synth = (function () {
 				envelope.setSustain( settings.sustain );
 
 				vco.connect( vca );
-				vca.connect( pannerGain );
-			
-				pannerGain.connect( panner );
+				vca.connect( filter );
+				filter.connect( object.merger, 0, settings.id );
 
-				panner.panningModel = 'equalpower';
-				panner.setPosition( settings.pan, 0, 1 - Math.abs( settings.pan ) );
-				panner.connect( chSum );
+				filter.setFrequency( settings.filter.frequency );
+				filter.setType( settings.filter.type );
+				filter.setQ( settings.filter.q );
 
 				envelope.connect( vca.amplitude );
 				envelope.trigger();
@@ -237,7 +294,7 @@ var Synth = (function () {
 					vco: vco,
 					vca: vca,
 					envelope: envelope,
-					panner: panner
+					panner: pannerGain
 				}
 
 			},
@@ -247,9 +304,9 @@ var Synth = (function () {
 				var thisOscillator,
 					stopTime;
 
-				for ( var i = Oscillators[ frequency ].length - 1; i >= 0; i-- ) {
+				for ( var i = Oscillators[ frequency ].oscillators.length - 1; i >= 0; i-- ) {
 
-					thisOscillator = Oscillators[ frequency ][ i ];
+					thisOscillator = Oscillators[ frequency ].oscillators[ i ];
 					stopTime = context.currentTime + thisOscillator.envelope.attackTime + thisOscillator.envelope.releaseTime;
 
 					if ( thisOscillator.envelope.sustain === 1 ) {
